5cf7553dedadda4acd84b0dc1fca884ce2dc4ef1
priyankasaxena19 <priyanka.saxena.techexactly@gmail.com>
2024-10-23 19:21:27 +0530

    added

--- src/todolist/todolist.jsx
+++ src/todolist/todolist.jsx
@@ -1,22 +1,25 @@
 import { React, useState } from "react";
 import "./todolist.css";
 export default function TodoList() {
-    const[darkMode, setDarkMode] =useState(false);
+    const[darkMode, setDarkMode] =useState(localStorage.getItem("mode")  || false);
     const[changesText , setText] = useState('');
-    const[todos,setTodo]= useState([]);
+    const[todos,setTodo]= useState(localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []);
     
     //methods 
 
     const saveChanges = e => {
-        setText(e.target.value)
+        setText(e.target.value);
+
     }
     const saveTodo = e => {
         todos.push(changesText);
         setTodo(todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
         setText("");
         console.log(todos);
     }
     const changeMode = e => {
+        localStorage.setItem('mode',!darkMode);
         setDarkMode(!darkMode);
     }
     const modeText = darkMode ? "Dark Mode" : "Light Mode";
